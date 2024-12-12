562d4e87132f0d883bb82fda4e55583ca0ade25c
Gabriel-Ricci <gabrielr244@nycstudents.net>
2024-12-12 14:33:42 +0000

    b

---
+++ Painting.java
@@ -0,0 +1,10 @@
+public class Painting {
+    private int height;
+    private int width;
+    private String color;
+
+    public int area(width, height) {
+        int area = width * height;
+        return area;
+    }
+}
---
+++ main.java/main.java
@@ -0,0 +1,4 @@
+public static void main(String args[]) {
+    int a = area(5, 7);
+    System.out.println(a);
+}
